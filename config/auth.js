22842466570b508b2bd9f24f3034f003c629f9d0
Matthew <mfrench71@googlemail.com>
2018-11-27 09:54:08 +0000

    Initial commit

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = 
+{
+    'consumer_key'        : '3LPWbNM3ssnBrpIqoJhgEawJJ',
+    'consumer_secret'     : 'T2Lpe9I8RwyFIqLKWYrGxQrDtmhbHjJHeftR3Qu9fdy50Kcwm8',
+    'access_token'        : '3309595677-GGsOkCr2Apt3VHd2ifgVfn5okqyO5KIui8Euvlm',
+    'access_token_secret' : 'KbrUQxq7TuyxLMivzmJJZpO2VBCkkNxk0Ci0WSSD5WOin',
+    'callbackURL'         : 'http://localhost:8080/auth/twitter/callback',
+    'screen_name'		  : 'Ethan99french'
+};
