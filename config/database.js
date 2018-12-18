8fe6b7e76ce72e03227f6c6f1396193d7e98aefb
Matthew <mfrench71@googlemail.com>
2018-12-18 12:43:00 +0000

    DB connection

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
-    'url' : 'mongodb://localhost/test' // local MongoDB
-    // 'url' : 'mongodb://mfrench:Mf11091971!@ds039950.mlab.com:39950/mftest' // mLab MongoDB
+    // 'url' : 'mongodb://localhost/test' // local MongoDB
+    'url' : 'mongodb://mfrench:Mf11091971!@ds039950.mlab.com:39950/mftest' // mLab MongoDB
 };
